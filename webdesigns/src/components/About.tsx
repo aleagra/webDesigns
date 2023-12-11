a35a0e41e6099de63a65448ee3bf7bb448e761cf
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-12-11 19:00:15 -0300

    Update About.tsx

--- webdesigns/src/components/About.tsx
+++ webdesigns/src/components/About.tsx
@@ -4,8 +4,31 @@ const About: React.FC = () => {
 	return (
 		<>
 			<section>
-				<div className="w-[50%] flex border border-black">
-                <Image src={foto} alt="Your Alt Text" width={800} height={300} />
+				<div className="w-full flex flex-col justify-center text-center">
+					{' '}
+					<h1 className='font-bold text-5xl mt-10'>¿Quienes Somos?</h1>
+					<div className='w-full p-20 flex items-center gap-5'>
+						<div className="w-[50%] flex border border-black ">
+							<Image src={foto} alt="Your Alt Text" width={435} height={300} />
+							<div className="flex flex-col justify-center text-center p-6">
+							<h1 className='text-2xl mb-2 font-semibold'>Alejandro Agra</h1>
+								<h2>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas a veniam itaque voluptatem adipisci nemo
+									perspiciatis natus iste quibusdam deleniti tenetur ea animi pariatur ipsam, ipsa ad neque libero nisi!
+								</h2>
+							</div>
+						</div>
+						<div className="w-[50%] flex border border-black ">
+							<div className="flex flex-col justify-center text-center p-6">
+								<h1 className='text-2xl mb-2 font-semibold'>Moscoloni Juan Pablo</h1>
+								<h2>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas a veniam itaque voluptatem adipisci nemo
+									perspiciatis natus iste quibusdam deleniti tenetur ea animi pariatur ipsam, ipsa ad neque libero nisi!
+								</h2>
+							</div>
+							<Image src={foto} alt="Your Alt Text" width={435} height={300} />
+						</div>
+					</div>
 				</div>
 			</section>
 		</>
